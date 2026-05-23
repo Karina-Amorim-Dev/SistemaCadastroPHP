4f7a527fd3af473587d10cfffb97f44734d469a7
Karina <karina.a,orim.etec@gmail.com>
2026-05-23 15:47:06 -0300

    feat: Criando o index de Produtos

--- Produtos/index.php
+++ Produtos/index.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="UTF-8">
         <title>CRUD PHP</title>
-        <link rel="stylesheet" href="estilo.css">
+        <link rel="stylesheet" href="../estilo.css">
     </head>
 
     <body>
