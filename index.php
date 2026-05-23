76b706ceb06a0cdb5fab8d9104b0585f0756b54a
Karina <karina.a,orim.etec@gmail.com>
2026-05-23 15:57:00 -0300

    feat: Criando o Index Principal

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema de Cadastros</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+
+<div class="container">
+
+    <h1>Sistema de Cadastros</h1>
+
+    <ul>
+        <li><a href="produtos/index.php">Produtos</a></li>
+        <li><a href="clientes/index.php">Clientes</a></li>
+        <li><a href="funcionarios/index.php">Funcionários</a></li>
+        <li><a href="livros/index.php">Livros</a></li>
+        <li><a href="veiculos/index.php">Veículos</a></li>
+        <li><a href="filmes/index.php">Filmes</a></li>
+    </ul>
+
+</div>
+
+</body>
+</html>
