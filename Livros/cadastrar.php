7b8e7c3d7e0f2eaf20c81756ffd85e07e1a65ccd
Karina <karina.a,orim.etec@gmail.com>
2026-05-24 19:17:31 -0300

    feat: Manutenção no código do Cadastrar de Livros

--- Livros/cadastrar.php
+++ Livros/cadastrar.php
@@ -7,9 +7,8 @@ $formato = $_POST['formato'];
 $editora = $_POST['editora'];
 $categoria = $_POST['categoria'];
 $preco = $_POST['preco'];
-$ano_lancamento = $_POST['ano_lancamento'];
-$sql = "INSERT INTO livros(isbn, titulo, autor, formato, editora, categoria, preco, ano_lancamento)
-VALUES ('$isbn_id_liv', '$titulo', '$autor', '$formato', '$editora', '$categoria', '$preco', '$ano_lancamento')";
+$sql = "INSERT INTO livros(isbn, titulo, autor, formato, editora, categoria, preco)
+VALUES ('$isbn_id_liv', '$titulo', '$autor', '$formato', '$editora', '$categoria', '$preco')";
 if ($conexao->query($sql) === TRUE) {
 header('Location: index.php');
 } else {
